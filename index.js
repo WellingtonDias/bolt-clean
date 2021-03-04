df727fad32ecbb2c9dd81511f1d418477775fd3a
Wellington Dias <wellingtongomesdias@gmail.com>
2021-03-03 21:49:15 -0300

    [FIX] replace "helper" with "bolt"

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function(TASK,PATHS)
 	const paths = bolt.resolvePath(PATHS,{strict: false});
 	for (let path of paths)
 	{
-		if (helper.isDirectory(path)) fs.rmdirSync(path,{recursive: true});
+		if (bolt.isDirectory(path)) fs.rmdirSync(path,{recursive: true});
 		else fs.unlinkSync(path);
 	};
 };
